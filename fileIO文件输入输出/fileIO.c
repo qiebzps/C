a5dea7212b0b95197812da988911d7ac4a2e55e3
zps <1033239636@qq.com>
2019-07-13 00:44:28 +0800

    fileIO

---
+++ fileIO文件输入输出/fileIO.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+int main(void){
+    /* Accessing Files */
+    //FILE * fp = NULL;
+    //fp = fopen("fileIO.txt","r");
+    //char * str = "This is a try of Accessing Files\n";
+    //printf("%s",str);
+    //if(fp != NULL){
+    //    printf("Successful!\n");
+    //}
+    //if (fclose(fp) != EOF){
+    //    printf("Accessing files fclose is Successful!\n");
+    //}
+
+    /* Reading from a File */
+    //FILE * fp = NULL;
+    //fp = fopen("fileIO.txt","r");
+    //int c;
+    //while((c = getc(fp)) != EOF){
+    //    //printf("%c",c);
+    //    //putchar(c);
+    //    //putc(c,stdout);
+    //}
+    //if (fclose(fp) != EOF){
+    //    printf(" Reading form a file fclose is Successful!\n");
+    //}
+
+
+
+    return 0;
+}
